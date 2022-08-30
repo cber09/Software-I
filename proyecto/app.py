cf6fffba894810375c8455fc7e6c8e6b46ae2484
Jhoel Sequeira <56564490+Jhoel-Sequeira@users.noreply.github.com>
2022-08-30 17:52:16 -0600

    Funcionalidad
    -Login
    -Registrarse
    -Pantalla Home segun rol

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,9 +1,12 @@
+import datetime
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, sessions, url_for, session, send_file
 from jinja2 import Environment
+from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 
 db1 = SQL("sqlite:///Veterinaria.db")
+app.secret_key = "super secret key"
 
 @app.route('/')
 def Index():
@@ -12,10 +15,67 @@ def Index():
 @app.route('/asociate')
 def asociate():
     return render_template('asociate.html')
-    # usuario nuevo
-@app.route('/nuevou')
+@app.route('/login',methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        usuario = request.form['loginUser']
+        Contraseña = request.form['loginPassword']
+        if usuario == "" or Contraseña == "":
+            return render_template('asociate.html', errorlogin=1)
+        else:
+            rows = db1.execute("SELECT * FROM Credenciales Where Usuario=:username",
+                               username=usuario)
+            
+            if len(rows) == 0 or not check_password_hash(rows[0]["Contraseña"], Contraseña):
+                return render_template('asociate.html', errorlogin=1)
+            else:
+                # Estas consultas son para mostrar la lista de personas y su asistencia
+                
+                empleadoRol = db1.execute(
+                    "select rol.* from Credenciales as cred INNER JOIN Roles as rol ON cred.Rol = rol.Id_Rol WHERE cred.Usuario =:u", u=usuario)
+                empleado = db1.execute(
+                    "select * from Usuarios as u inner join Credenciales as cred ON u.IdCredenciales = cred.Id_Credenciales Where cred.Usuario =:u", u=usuario)
+
+                # # Recordar el usuario y rol que se logeo
+                session["user"] = empleado[0]["Usuario"]
+                session["usercom"] = empleado[0]["Nombres"]
+                session["user_Id"] = empleado[0]["Id_Usuario"]
+                session["userrole"] = empleadoRol[0]["Id_Rol"]
+                session["nombrerol"] = empleadoRol[0]["NombreRol"]
+                
+                return redirect(url_for('home'))
+    return render_template('index.html')
+#DESLOGUEO
+@app.route('/deslog')
+def deslog():
+    session.clear()
+    return render_template('index.html',rol ="",nombre ="" )
+#HOME AQUI
+@app.route('/home')
+def home():
+    return render_template('index.html',rol =session["nombrerol"],nombre =session["usercom"] )
+# usuario nuevo
+@app.route('/nuevou',methods=["GET", "POST"])
 def nuevou():
-    return render_template('nuevous.html')
+    if request.method == "POST":
+        nombres = request.form['nombres']
+        apellidos = request.form['apellidos']
+        direccion = request.form['direccion']
+        telefono = request.form['telefono']
+        celular = request.form['celular']
+        usuario = request.form['loginUser']
+        contraseña = request.form['loginPassword']
+        verificacion = db1.execute('SELECT * from credenciales Where Usuario = :u',u = usuario)
+        db1.execute("INSERT INTO Credenciales VALUES(NULL,:usu,:passw,3)",usu = usuario,passw = generate_password_hash(contraseña))
+        credenciales = db1.execute('select Id_Credenciales from Credenciales  order by Id_Credenciales desc limit 1')
+        db1.execute("INSERT INTO Usuarios VALUES(NULL,:name,:lastna,:tel,:cel,null,:dir,1,:cred)",
+                    name=nombres , lastna=apellidos , tel = telefono,cel = celular,dir=direccion,cred = credenciales[0]['Id_Credenciales'])
+        
+        
+        return redirect(url_for('asociate'))
+    else:
+        return render_template('nuevous.html')
 #CONTACTOS
 @app.route('/contactos')
 def contactos():
